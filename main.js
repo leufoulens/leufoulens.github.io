b2cde6e22c44303a58b0850391afe981b53078a0
leufoulens <111640037+leufoulens@users.noreply.github.com>
2022-08-20 16:34:31 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+let button = document.getElementById("main-button")
+let turnik = document.getElementById("turnik")
+let counter = document.getElementById("main-counter")
+let mainContainer = document.getElementById("main-container")
+let counterValue = 0
+
+button.onmousedown = function() {
+    if(counterValue < 29){
+    turnik.style.backgroundPosition = "100%"
+    counterValue++
+    counter.innerHTML = counterValue + " из 30"
+} else {
+    let randomValue = Math.floor(Math.random()*2)
+    console.log(randomValue)
+    document.body.style.background = "rgba(0, 0, 0, 0.363)"
+    document.getElementById("overlay").style.display = "block"
+    if(randomValue == 1){
+        console.log("Успех!!")
+        document.getElementById("alert-box-text").innerHTML = "<h1>УСПЕХ!</h1>"
+        document.getElementById("alert-box-image").style.backgroundPosition = "0%"
+        document.getElementById("alert-box-button").innerHTML = "Накачаться еще раз"
+    } else {
+        console.log("Провал!!(")
+        document.getElementById("alert-box-text").innerHTML = "<h1>ПРОВАЛ!</h1>"
+        document.getElementById("alert-box-image").style.backgroundPosition = "100%"
+        document.getElementById("alert-box-button").innerHTML = "Попробовать еще раз"
+    }
+}
+}
+button.onmouseup = function() {
+    turnik.style.backgroundPosition = "0%"
+}
+
